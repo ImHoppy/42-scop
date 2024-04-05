4f6702f150716b957da7bbcd0c0b7d744f7666f2
Hoppy <dev.hoppy@gmail.com>
2024-04-05 10:37:27 +0200

    find_glslc fix powershell

--- find_glslc.sh
+++ find_glslc.sh
@@ -3,33 +3,40 @@ set -e
 
 BIN=""
 
-host=$(hostname -f)
+if [ -n "$PSVersionTable" ]; then
+    # Windows
+    command_exist() { pwsh -Command "(Get-Command -CommandType Application '$1').Path"; }
+else
+    command_exist() { command -v "$1"; }
+fi
+
+host=$(hostname)
 suffix=".42paris.fr"
 
-if [ -e "$(which glslc)" ]; then
+if [ -e "$(command_exist glslc)" ]; then
 	BIN="glslc"
-elif [ -e "$(which glslc.exe)" ]; then
+elif [ -e "$(command_exist glslc.exe)" ]; then
     # support WSL
-    BIN="$(which glslc.exe)"
+    BIN="$(command_exist glslc.exe)"
 elif [[ ! $host == *"$suffix" ]]; then
     >&2 echo "Cannot find glslc binary. Make sure you have VulkanSDK installed and added to PATH."
     exit 1
+else
+	SOURCE_DIR=$HOME/goinfre/shaderc
+	BUILD_DIR=$HOME/goinfre/shaderc_build
+
+	if ! [ -d $BUILD_DIR ]; then
+		git clone https://github.com/google/shaderc $SOURCE_DIR
+		cd $SOURCE_DIR
+		./utils/git-sync-deps
+
+		mkdir -p $BUILD_DIR
+		cd $BUILD_DIR
+		cmake -GNinja -DCMAKE_BUILD_TYPE=Release $SOURCE_DIR
+		ninja
+		# ctest # optional
+	fi
+	BIN=$BUILD_DIR/glslc/glslc
 fi
-
-SOURCE_DIR=$HOME/goinfre/shaderc
-BUILD_DIR=$HOME/goinfre/shaderc_build
-
-if ! [ -d $BUILD_DIR ]; then
-	git clone https://github.com/google/shaderc $SOURCE_DIR
-	cd $SOURCE_DIR
-	./utils/git-sync-deps
-
-	mkdir -p $BUILD_DIR
-	cd $BUILD_DIR
-	cmake -GNinja -DCMAKE_BUILD_TYPE=Release $SOURCE_DIR
-	ninja
-	# ctest # optional
-fi
-BIN=$BUILD_DIR/glslc/glslc
 
 echo "$BIN"
